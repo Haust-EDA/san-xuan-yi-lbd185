2064ff19c5ff44ccdca81e98b9a0611353a31ac6
lbd185 <1809994884@qq.com>
2024-11-20 19:02:26 +0800

    Update main.c

--- main.c
+++ main.c
@@ -16,3 +16,18 @@ int main()
 	return 0;
 }
 //倒三角
+#define  _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int main()
+{
+	int i, j;
+	for (i = 9; i >= 1; i--)
+	{
+		for (j = i; j >= 1; j--)
+		{
+			printf("%d*%d=%-4d", j, i, i * j);
+		}
+		printf("\n");
+	}
+	return 0;
+}
