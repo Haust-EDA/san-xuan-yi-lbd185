ae4f92f53683e745ff89be11d3813e9d322c400f
lbd185 <1809994884@qq.com>
2024-11-20 18:57:57 +0800

    Update main.c

--- main.c
+++ main.c
@@ -1,2 +1,18 @@
 //请在下方输入你的代码：
+//正三角
+#define  _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+int main()
+{
+	int i, j;
+	for (i = 1; i <= 9; i++)
+	{
+		for (j = 1;j <= i;j++)
+		{
+			printf("%d*%d=%-4d", j, i, i * j);
+		}
+		printf("\n");
+	}
+	return 0;
+}
+//倒三角
